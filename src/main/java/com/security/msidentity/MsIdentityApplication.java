1ce1d521491c8673980f8bf06bbafeda1dba2107
Rashid R. Fasadov <Rashid.Fasadov@kapitalbank.az>
2023-01-05 16:44:27 +0400

    Add dependency

---
+++ src/main/java/com/security/msidentity/MsIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.security.msidentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsIdentityApplication.class, args);
+	}
+
+}
